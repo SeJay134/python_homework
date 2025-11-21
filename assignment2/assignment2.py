b9c85242774448b6f2db87cae25a57232bbcbd9b
SeJay134 <patrushev.s.job@gmail.com>
2025-11-21 12:45:47 -0500

    added task 2

---
+++ assignment2/assignment2.py
@@ -0,0 +1,28 @@
+# Task 2
+import csv
+
+def read_employees():
+    dict_employees = {}
+    list_store = []
+
+    try:
+        with open('../csv/employees.csv', 'r', newline='') as file:
+            reader = csv.reader(file)
+            for row in reader:
+
+                if "fields" not in dict_employees:
+                    dict_employees["fields"] = row
+                else:
+                    list_store.append(row)
+
+        dict_employees["rows"] = list_store
+        return dict_employees
+
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+employees = read_employees()
+print(employees)
+
+# Task 3
