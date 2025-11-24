0473fbb76748c3222462f54da6c71a477c932c80
Sergei <patrushev.s.job@gmail.com>
2025-11-23 21:38:55 -0500

    added tasks

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,26 +1,29 @@
-# Task 2
+# Utilities
 import csv
 
-def read_employees():
-    dict_employees = {}
-    list_store = []
+def open_csv(csvName):
+    tmp = {}
+    tmp2 = []
 
     try:
-        with open('../csv/employees.csv', 'r', newline='') as file:
+        with open('../csv/'+csvName, 'r', newline='') as file:
             reader = csv.reader(file)
             for row in reader:
-
-                if "fields" not in dict_employees:
-                    dict_employees["fields"] = row
+                if "fields" not in tmp:
+                    tmp["fields"] = row
                 else:
-                    list_store.append(row)
+                    tmp2.append(row)
 
-        dict_employees["rows"] = list_store
-        return dict_employees
+        tmp["rows"] = tmp2
+        return tmp
 
     except Exception as e:
         print("Error:", e)
         return None
+
+# Task 2
+
+read_employees = lambda: open_csv("employees.csv")
 
 employees = read_employees()
 print(employees)
@@ -28,7 +31,6 @@ print(employees)
 # Task 3
 def column_index(name_column):
     x = employees['fields'].index(name_column)
-    print(x)
     return x
 
 employee_id_column = column_index('employee_id')
@@ -60,3 +62,93 @@ def employee_find_2(employee_id):
     return matches
 
 print(employee_find_2(1))
+
+# Task 7
+def sort_by_last_name():
+    employees["rows"].sort(key=lambda row : row[column_index("last_name")])
+    return employees["rows"]
+
+# Task 8
+def employee_dict(employee_row):
+    tmp = {}
+    for i in employees['fields']:
+        if(i != "employee_id"):
+            tmp[i] = employee_row[employees['fields'].index(i)]
+    return tmp
+
+print(employee_dict(employees["rows"][0]))
+
+# Task 9
+def all_employees_dict():
+    tmp = {}
+    for i in employees['rows']:
+        tmp[i[0]] = employee_dict(i)
+    return tmp
+
+print(all_employees_dict())
+
+# Task 10
+import os
+os.environ['THISVALUE'] = 'ABC'
+
+get_this_value = lambda: os.getenv("THISVALUE")
+
+print(get_this_value())
+
+# Task 11
+import custom_module
+
+set_that_secret = lambda secret: custom_module.set_secret(secret)
+
+set_that_secret("secret")
+print(custom_module.secret)
+
+# Task 12
+
+def read_minutes():
+    tmp1 = open_csv("minutes1.csv")
+    tmp = []
+    for i in tmp1["rows"]:
+        tmp.append(tuple(i))
+    tmp1["rows"] = tmp
+    
+    tmp2 = open_csv("minutes2.csv")
+    tmp = []
+    for i in tmp2["rows"]:
+        tmp.append(tuple(i))
+    tmp2["rows"] = tmp
+
+    return tmp1, tmp2
+
+global minutes1, minutes2
+minutes1, minutes2 = read_minutes()
+
+# Task 13
+
+create_minutes_set = lambda: set(minutes1["rows"]).union(set(minutes2["rows"]))
+
+global minutes_set
+minutes_set = create_minutes_set()
+print(minutes_set)
+
+# Task 14
+from datetime import datetime
+
+create_minutes_list = lambda: list(map(lambda x: (x[0], datetime.strptime(x[1], "%B %d, %Y")), list(minutes_set)))
+
+global minutes_list
+minutes_list = create_minutes_list()
+print(minutes_list)
+
+# Task 15
+
+def write_sorted_list():
+    minutes_list.sort(key=lambda row : row[1])
+    tmp = list(map(lambda x: (x[0], datetime.strftime(x[1], "%B %d, %Y")), minutes_list))
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(minutes1["fields"])
+        writer.writerows(tmp)
+    return tmp
+
+write_sorted_list()
